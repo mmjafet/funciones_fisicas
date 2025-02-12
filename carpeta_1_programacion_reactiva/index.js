1d038bbe7595a2bdf44a7afe75729474bbd55f2d
mmjafet <mmjafet01dic02@gmail.com>
2025-02-12 16:12:30 -0600

    Add greeting event handler to index.js for DOM manipulation

--- carpeta_1_programacion_reactiva/index.js
+++ carpeta_1_programacion_reactiva/index.js
@@ -1,2 +1,10 @@
 const mensaje = document.getElementById('mensaje');
 const boton = document.getElementById('boton');
+
+
+
+const eventoSaludar = () => {
+    let texto = "hola desde js con este Dom manipulado";
+    mensaje.innerText = texto;
+    mensaje.innerText += texto;
+}
