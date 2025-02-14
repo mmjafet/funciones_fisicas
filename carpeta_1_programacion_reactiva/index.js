886a083cc106d531513ee6706e7e18ca972636bd
mmjafet <mmjafet01dic02@gmail.com>
2025-02-14 07:49:39 -0600

    Add SweetAlert2 for result display and parse numeric inputs in calculator

--- carpeta_1_programacion_reactiva/index.js
+++ carpeta_1_programacion_reactiva/index.js
@@ -13,8 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
         // Crear un objeto FormData con los datos del formulario
         const data = new FormData(evt.target);
         // Obtener los valores de los campos del formulario
-        const num1 = data.get("numero1");
-        const num2 = data.get("numero2");
+        //definir que son numeros con float o int parseando 
+        //parse float o parse int
+        const num1 = parseFloat(data.get("numero1"));
+        const num2 = parseFloat(data.get("numero2"));
 
         // Sumar los valores obtenidos
         let result = +num1 + +num2;
@@ -22,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(`num1: ${num1}, result: ${result}, num2: ${num2}`);
         // Mostrar el resultado en el elemento de resultado
         alert(`La suma de ${num1} + ${num2} es: ${result}`);
+        Swal.fire("La suma de ${num1} + ${num2} es: ${result}");
     };
 
     // Añadir el evento de envío al formulario
