600b3cec606205c27b5136ff28db5c8ba08c995a
mmjafet <mmjafet01dic02@gmail.com>
2025-02-13 21:47:08 -0600

    Refactor index.html and index.js to implement a calculator form; add styling in style.css

---
+++ 2_prpgramacion/index.js
@@ -0,0 +1,11 @@
+const formulario = document.getElementById('calculadora')
+
+const eventoFormulario = (evt)=>{
+    evt.preventDefault()}
+
+formulario.addEventListener('submit', eventoFormulario)
+
+const calcular = (evt)=>{
+
+    
+}
--- carpeta_1_programacion_reactiva/index.js
+++ carpeta_1_programacion_reactiva/index.js
@@ -1,10 +1,12 @@
 const mensaje = document.getElementById('mensaje');
-const boton = document.getElementById('boton');
+const boton = document.getElementById('btn-saludo');
 
 
 
 const eventoSaludar = () => {
-    let texto = "hola desde js con este Dom manipulado";
-    mensaje.innerText = texto;
+    let texto = "hola desde js con este Dom <a> manipulado </a>";
+    
     mensaje.innerText += texto;
-}
+    }
+
+boton.addEventListener('click', eventoSaludar);
