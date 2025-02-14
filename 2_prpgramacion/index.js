d7c6b5204bab6da60daf98152126b0221984d936
mmjafet <mmjafet01dic02@gmail.com>
2025-02-13 22:39:54 -0600

    Add console logging and update button to call calcular function on submit

--- 2_prpgramacion/index.js
+++ 2_prpgramacion/index.js
@@ -8,6 +8,8 @@ formulario.addEventListener('submit', eventoFormulario)
 const calcular = ()=>{
     let num1 = document.getElementById('num1').value
     let num2 = document.getElementById('num2').value
+    console.log(num1)
+    console.log(num2)
 
     suma = parseFloat(num1) + parseFloat(num2)
 
