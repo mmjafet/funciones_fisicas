163b97f2ce0ca7da3f52d16a6b581ea171d1cf5e
mmjafet <mmjafet01dic02@gmail.com>
2025-02-13 21:52:47 -0600

    Add sum calculation and alert in calcular function in index.js

--- 2_prpgramacion/index.js
+++ 2_prpgramacion/index.js
@@ -6,6 +6,11 @@ const eventoFormulario = (evt)=>{
 formulario.addEventListener('submit', eventoFormulario)
 
 const calcular = (evt)=>{
+    let num1 = document.getElementById('num1').value
+    let num2 = document.getElementById('num2').value
 
+    suma = (num1) + parseInt(num2)
+
+    alert(`La suma de los numeros es: ${suma}`)
     
 }
