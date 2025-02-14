2fbe9d1b245ecce5d24e06fe0d1e01a0caba8126
mmjafet <mmjafet01dic02@gmail.com>
2025-02-13 21:55:25 -0600

    Fix sum calculation in calcular function to use parseFloat for numeric inputs

--- 2_prpgramacion/index.js
+++ 2_prpgramacion/index.js
@@ -9,7 +9,7 @@ const calcular = ()=>{
     let num1 = document.getElementById('num1').value
     let num2 = document.getElementById('num2').value
 
-    suma = (num1) + parseInt(num2)
+    suma = parseFloat(num1) + parseFloat(num2)
 
     alert(`La suma de los numeros es: ${suma}`)
     
