a6b3a9ee785fbb418169e7eb575873adbf3389f2
mmjafet <mmjafet01dic02@gmail.com>
2025-02-19 16:24:42 -0600

    Add task creation logic; trim input and include task description in new task object

--- programa3/index.js
+++ programa3/index.js
@@ -1,10 +1,21 @@
+var con = 1;
+var data = [];
 document.addEventListener('DOMContentLoader', () => {
   const formulario = document.getElementById('formulario');
   const tarea = document.getElementById('tarea');
 
    formulario.addEventListener('submit', (evt) => {
     evt.preventDefault();    
-    const task = tareaInput.value;
+    const task = tareaInput.value.trim();
+
+    const  newTask = {
+        idCount: con++,
+        task: task,
+        description : description,
+        status: false,
+        }
+
+    data.push(newTask);
   });
 
   
