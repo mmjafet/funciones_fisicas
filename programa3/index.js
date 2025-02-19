b018679cec299b9bbf76a406b174bbb5e0450355
mmjafet <mmjafet01dic02@gmail.com>
2025-02-19 16:14:40 -0600

    Fix event listener for task submission; add task input retrieval from the form

--- programa3/index.js
+++ programa3/index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoader', () => {
   const formulario = document.getElementById('formulario');
+  const tarea = document.getElementById('tarea');
 
    formulario.addEventListener('submit', (evt) => {
     evt.preventDefault();    
-    
+    const task = tareaInput.value;
   });
 
   
