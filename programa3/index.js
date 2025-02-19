55b173638b115ceddf1e03785bbb4938642c02c4
mmjafet <mmjafet01dic02@gmail.com>
2025-02-19 16:12:11 -0600

    Refactor task form and styles; update input IDs for consistency and enhance layout with new CSS

---
+++ programa3/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoader', () => {
+  const formulario = document.getElementById('formulario');
+
+   formulario.addEventListener('submit', (evt) => {
+    evt.preventDefault();    
+    
+  });
+
+  
+});
